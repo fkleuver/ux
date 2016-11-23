b607a8cd567775a599b95b6c603655574a956298
Fred Kleuver <fred@avurad.nl>
2016-11-23 21:41:01 +0100

    feat(ux-select): add theme

---
+++ src/select/ux-select-theme.ts
@@ -0,0 +1,13 @@
+import {styles} from '../styles/decorators';
+
+@styles()
+export class UxSelectTheme {
+    public inDuration = 300;
+    public outDuration = 225;
+    public constrain_width = true;
+    public hover = false;
+    public gutter = 0;
+    public beloworigin = false;
+    public alignment = 'left';
+    public stopPropagation = false;
+}
--- src/select/ux-select.ts
+++ src/select/ux-select.ts
@@ -1,9 +1,33 @@
-import {customElement, bindable} from 'aurelia-templating';
+import {customElement, bindable, ViewResources, View, processAttributes} from 'aurelia-templating';
+import {inject} from 'aurelia-dependency-injection';
+import {StyleEngine} from '../styles/style-engine';
+import {Themable} from '../styles/themable';
+import {processDesignAttributes} from '../designs/design-attributes';
 
+@inject(ViewResources, StyleEngine)
 @customElement('ux-select')
-export class UxSelect {
+@processAttributes(processDesignAttributes)
+export class UxSelect implements Themable  {
+  @bindable public theme = null;
   @bindable public options = [];
   @bindable public value = null;
 
-  public select: HTMLSelectElement;
+  public view: View;
+  private select: HTMLSelectElement;
+
+  constructor(public resources: ViewResources, private styleEngine: StyleEngine) {}
+  
+  public created(_: any, myView: View) {
+    this.view = myView;
+  }
+
+  public bind() {
+    if (this.theme) {
+      this.styleEngine.applyTheme(this, this.theme);
+    }
+  }
+
+  public themeChanged(newValue: any) {
+    this.styleEngine.applyTheme(this, newValue);
+  }
 }
