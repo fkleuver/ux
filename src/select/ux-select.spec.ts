86ed13e0179935b8a59cae808ff60e86fd80fcf5
Fred Kleuver <fred@avurad.nl>
2016-11-24 04:44:15 +0100

    feat(ux-select): handle different types of arrays for options

--- src/select/ux-select.spec.ts
+++ src/select/ux-select.spec.ts
@@ -3,7 +3,8 @@ import { StageComponent } from 'aurelia-testing';
 import { bootstrap } from 'aurelia-bootstrapper';
 import { UxSelect } from './ux-select';
 
-declare type Option = { text: string, value: any };
+type Option = { text: string, value: any };
+type BindingContext = { options: any[], value: any, textProperty: string, valueProperty: string, theme: any };
 
 interface TComponentTester<T> {
     bind(): Promise<void>;
@@ -22,15 +23,12 @@ const resources = [
 ];
 
 
-function arrange(options: any[], value: any, bind: boolean, attach: boolean): Promise<TComponentTester<UxSelect>> {
-    let opt = options;
-    let val = value;
-
+function arrange(bindingContext: BindingContext, bind: boolean, attach: boolean): Promise<TComponentTester<UxSelect>> {
     return Promise
         .resolve(StageComponent
             .withResources(resources)
             .inView(view)
-            .boundTo({ options: opt, value: val })
+            .boundTo(bindingContext)
             .manuallyHandleLifecycle())
         .then((c) => {
             return c.create(bootstrap).then(() => Promise.resolve(c));
@@ -51,40 +49,133 @@ function arrange(options: any[], value: any, bind: boolean, attach: boolean): Pr
         });
 };
 
-describe('ux-select', () => {
-    let options: Option[];
+function assertViewModelCorrect(uxSelect: UxSelect, bindingContext: BindingContext): void {
+    expect(uxSelect.value).toBe(bindingContext.value);
+    expect(uxSelect['innerValue']).toBe(bindingContext.value);
+    expect(uxSelect.options).toBe(bindingContext.options);
+    for (let option of bindingContext.options) {
+        expect(uxSelect['innerOptions'][bindingContext.options.indexOf(option)]).toBe(option);
+    }
+};
 
-    beforeAll(() => {
-        options = [
-            { text: 'Option 1', value: 1 },
-            { text: 'Option 2', value: 2 },
-            { text: 'Option 3', value: 3 }
-        ];
+function assertViewCorrect(uxSelect: UxSelect, bindingContext: BindingContext, optionsAreWrapped: boolean): void {
+    let selectElement = <HTMLSelectElement>uxSelect['selectElement'];
+    if (optionsAreWrapped) {
+        let options = bindingContext.options;
+        let selectedOption = bindingContext.options.find((x) => uxSelect['defaultMatcher'](x, bindingContext.value));
+        expect(selectElement.value).toBe(selectedOption);
+    } else {
+        let expectedValue = uxSelect.textProperty ? bindingContext.value[uxSelect.textProperty] : bindingContext.value;
+        expect(selectElement.value).toBe(expectedValue);
+    }
+
+    for (let option of bindingContext.options) {
+        let optionElement = selectElement.options[bindingContext.options.indexOf(option)];
+        let expectedText = uxSelect.textProperty && option[uxSelect.textProperty] ? option[uxSelect.textProperty] : option;
+        expect(optionElement.innerHTML).toContain(expectedText);
+
+    }
+};
+
+describe('ux-select', () => {
+
+    describe('bind()', () => {
+        describe('when options is null', () => {
+            let bindingContext: BindingContext;
+
+            it('does not throw', (done) => {
+                bindingContext = <any>{};
+                bindingContext.options = <any>null
+                bindingContext.value = 'Foo';
+
+                arrange(bindingContext, false, false)
+                    .then((c) => {
+                        let err: any;
+                        try {
+                            c.bind();
+                        }
+                        catch (e) {
+                            err = e;
+                        }
+                        expect(err).toBeUndefined();
+                        done();
+                    });
+
+            });
+        });
+
+        describe('when options is not null and not an array', () => {
+            let sut: UxSelect;
+
+            it('throws', () => {
+                sut = new UxSelect(<any>null, <any>null, <any>null);
+                let err: any;
+                try {
+                    sut.optionsChanged(<any>5);
+                }
+                catch (e) {
+                    err = e;
+                }
+                expect(err.message).toBe('\'options\' must be an array');
+            });
+        });
     });
 
-    describe('when bound to three simple options with the first as it\'s value', () => {
+    describe('when bound to 3 objects with text+value properties', () => {
+        let bindingContext: BindingContext;
         let component: TComponentTester<UxSelect>;
 
-        beforeAll((done) => {
-            arrange(options, options[0].value, true, true)
+        beforeEach((done) => {
+            bindingContext = <any>{};
+            bindingContext.options = [
+                { text: 'Option 1', value: 1 },
+                { text: 'Option 2', value: 2 },
+                { text: 'Option 3', value: 3 }
+            ];
+            bindingContext.value = bindingContext.options[1];
+
+            arrange(bindingContext, true, true)
                 .then((c) => {
                     component = c;
                     done();
                 });
         });
 
-        it('viewModel.value should be equal to first option\'s value', () => {
-            expect(component.viewModel.value).toBe(options[0].value);
+        it('binds correctly', () => {
+            assertViewModelCorrect(component.viewModel, bindingContext);
         });
 
-        it('viewModel.options should be equal to passed in options', () => {
-            expect(component.viewModel.options).toBe(options);
+        it('renders correctly', () => {
+            assertViewCorrect(component.viewModel, bindingContext, false);
+        });
+    });
+
+    describe('when bound to 3 strings', () => {
+        let bindingContext: BindingContext;
+        let component: TComponentTester<UxSelect>;
+
+        beforeEach((done) => {
+            bindingContext = <any>{};
+            bindingContext.options = [
+                'Option 1',
+                'Option 2',
+                'Option 3'
+            ];
+            bindingContext.value = bindingContext.options[1];
+
+            arrange(bindingContext, true, true)
+                .then((c) => {
+                    component = c;
+                    done();
+                });
         });
 
-        it('the innerHTML should contain the text of all options', () => {
-            for (let opt of options) {
-                expect(component.viewModel.select.innerHTML).toContain(opt.text);
-            }
+        it('binds correctly', () => {
+            assertViewModelCorrect(component.viewModel, bindingContext);
+        });
+
+        it('renders correctly', () => {
+            assertViewCorrect(component.viewModel, bindingContext, true);
         });
     });
 });
--- src/select/ux-select.ts
+++ src/select/ux-select.ts
@@ -1,33 +1,176 @@
-import {customElement, bindable, ViewResources, View, processAttributes} from 'aurelia-templating';
-import {inject} from 'aurelia-dependency-injection';
-import {StyleEngine} from '../styles/style-engine';
-import {Themable} from '../styles/themable';
-import {processDesignAttributes} from '../designs/design-attributes';
+import { customElement, bindable, ViewResources, View, processAttributes } from 'aurelia-templating';
+import { bindingMode, observable, BindingEngine, Disposable } from 'aurelia-binding';
+import { inject } from 'aurelia-dependency-injection';
+import { StyleEngine } from '../styles/style-engine';
+import { Themable } from '../styles/themable';
+import { processDesignAttributes } from '../designs/design-attributes';
 
-@inject(ViewResources, StyleEngine)
+@inject(ViewResources, StyleEngine, BindingEngine)
 @customElement('ux-select')
 @processAttributes(processDesignAttributes)
-export class UxSelect implements Themable  {
-  @bindable public theme = null;
-  @bindable public options = [];
-  @bindable public value = null;
+export class UxSelect implements Themable {
 
-  public view: View;
-  private select: HTMLSelectElement;
-
-  constructor(public resources: ViewResources, private styleEngine: StyleEngine) {}
-  
-  public created(_: any, myView: View) {
-    this.view = myView;
-  }
-
-  public bind() {
-    if (this.theme) {
-      this.styleEngine.applyTheme(this, this.theme);
+    private defaultMatcher = (a: any, b: any) => {
+        if (this.textProperty) {
+            return a[this.textProperty] === b[this.textProperty];
+        } else {
+            return a === b;
+        }
     }
-  }
 
-  public themeChanged(newValue: any) {
-    this.styleEngine.applyTheme(this, newValue);
-  }
+    @bindable({ defaultBindingMode: bindingMode.oneTime })
+    public matcher: (a: any, b: any) => boolean = this.defaultMatcher;
+
+    @bindable({ defaultBindingMode: bindingMode.oneTime, defaultValue: 'text' })
+    public textProperty: string;
+
+    @bindable({ defaultBindingMode: bindingMode.oneTime, defaultValue: null })
+    public valueProperty: string;
+
+    @bindable({ defaultBindingMode: bindingMode.oneWay })
+    public theme: any = null;
+
+    @bindable({ defaultBindingMode: bindingMode.oneWay })
+    public options: any[] = <any>null;
+
+    @bindable({ defaultBindingMode: bindingMode.twoWay })
+    public value: any = null;
+
+    public view: View;
+    private selectElement: HTMLSelectElement;
+    private isAttached: boolean = false;
+    private innerOptions: any[] = [];
+    @observable private innerValue: any = null;
+    private optionsCollectionSubscription: Disposable;
+
+    constructor(
+        public resources: ViewResources,
+        private styleEngine: StyleEngine,
+        private bindingEngine: BindingEngine) {
+
+    }
+
+    public created(_: any, myView: View): void {
+        this.view = myView;
+    }
+
+    public bind(): void {
+        this.themeChanged(this.theme);
+        this.optionsChanged(this.options);
+        this.valueChanged(this.value);
+    }
+
+    public attached(): void {
+        this.isAttached = true;
+    }
+
+    public detached(): void {
+        this.isAttached = false;
+    }
+
+    public unbind(): void {
+        this.innerOptions = [];
+        this.innerValue = null;
+
+        if (this.optionsCollectionSubscription) {
+            this.optionsCollectionSubscription.dispose();
+            this.optionsCollectionSubscription = <any>null;
+        }
+    }
+
+    public themeChanged(newValue: any): void {
+        if (newValue) {
+            this.styleEngine.applyTheme(this, newValue);
+        }
+    }
+
+    public optionsChanged(newValue: any[]): void {
+        if (this.optionsCollectionSubscription) {
+            this.optionsCollectionSubscription.dispose();
+            this.optionsCollectionSubscription = <any>null;
+        }
+
+        this.innerOptions = [];
+
+        if (!newValue) {
+            return;
+        }
+
+        if (!isArray(newValue)) {
+            throw new Error('\'options\' must be an array');
+        }
+
+        this.optionsCollectionSubscription = this.bindingEngine
+            .collectionObserver(this.options)
+            .subscribe(this.optionsCollectionChanged);
+
+        for (let option of newValue) {
+            this.addOption(option);
+        }
+    }
+
+    private optionsCollectionChanged = (changeRecords: ChangeRecords) => {
+        for (let removedOption of changeRecords.removed) {
+            this.removeOption(removedOption);
+        }
+
+        for (let i = 0; i < changeRecords.addedCount; i++) {
+            let addedOption = this.options[changeRecords.index + i];
+            this.addOption(addedOption);
+        }
+    }
+
+    public valueChanged(newValue: any): void {
+        if (this.innerValue !== newValue) {
+            this.innerValue = newValue;
+        }
+    }
+
+    protected innerValueChanged(newValue: any): void {
+        if ((this.valueProperty ? this.value[this.valueProperty] : this.value) !== newValue) {
+            this.value = newValue;
+        }
+    }
+
+    private addOption(option: any): void {
+        if (isSimpleType(option) && this.valueProperty) {
+            let wrappedOption: any = {};
+            wrappedOption[this.valueProperty] = option;
+            wrappedOption[this.textProperty] = option.toString();
+            this.innerOptions.push(wrappedOption);
+        } else {
+            this.innerOptions.push(option);
+        }
+    }
+
+    private removeOption(option: any): void {
+        let idx: number;
+        if (isSimpleType(option)) {
+            let wrappedOption = this.innerOptions.find((x) => x[this.valueProperty] === option);
+            idx = this.innerOptions.indexOf(wrappedOption);
+        } else {
+            idx = this.innerOptions.indexOf(option);
+        }
+        this.innerOptions.splice(idx);
+    }
+}
+
+interface ChangeRecords {
+    addedCount: number;
+    index: number;
+    removed: any[];
+}
+
+function isSimpleType(value: any): boolean {
+    const type = Object.prototype.toString.call(value);
+    return type === '[object String]' ||
+        type === '[object Number]' ||
+        type === '[object Boolean]' ||
+        type === '[object Date]' ||
+        type === '[object RegExp]';
+}
+
+function isArray(value: any): boolean {
+    const type = Object.prototype.toString.call(value);
+    return type === '[object Array]';
 }
